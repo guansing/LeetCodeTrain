ec04dcf3ab7b59a881eb56626099dd3a73b22a22
1349621303 <1349621303@qq.com>
2021-03-22 13:30:41 +0800

    新增题目224

--- src/stack/stack_cal/test224.java
+++ src/stack/stack_cal/test224.java
@@ -1,51 +1,57 @@
 package stack.stack_cal;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
 
 public class test224 {
-    public static void main(String args[]) {
-        String s = "3+2-2";
-        Stack<Integer> stack = new Stack<>();
-        int num = 0;
-        char sign = '+';
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i))) {
-                num = num * 10 + (s.charAt(i) - '0');
-                System.out.println(num);
-            }
-            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == s.length() - 1) {
-                switch (sign) {
+    char LEFT = '(';
+    char RIGHT = ')';
+
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        Deque<Character> q = new ArrayDeque<>();
+        for (char c : s.toCharArray())
+            if (c != ' ') q.offer(c);
+        q.offer(' ');   // 确保最后一个 preNum 能被计算
+        return helper(q);
+    }
+    private int helper(Deque<Character> q) {
+        int res = 0;
+        int preNum = 0;
+        int curNum = 0;
+        char preOp = '+';
+
+        while (!q.isEmpty()) {
+            char c = q.poll();
+            if (Character.isDigit(c))  // 数字，比如 235, 568
+                curNum = curNum * 10 + c - '0';
+            else if (c == LEFT)        // 左括号，直接跟递归要结果
+                curNum = helper(q);
+            else {                    // 其他符号： + - * / )
+                switch(preOp) {
                     case '+':
-                        stack.push(+num);
+                        res += preNum;
+                        preNum = curNum;
                         break;
                     case '-':
-                        stack.push(-num);
+                        res += preNum;
+                        preNum = -curNum;
                         break;
                     case '*':
-                        int topNum1 = stack.peek();
-                        stack.pop();
-                        stack.push(topNum1 * num);
+                        preNum = preNum * curNum;
                         break;
-
                     case '/':
-                        int topNum2 = stack.peek();
-                        stack.pop();
-                        stack.push(topNum2 / num);
+                        preNum = preNum / curNum;
                         break;
                 }
-                sign = s.charAt(i);
-                num = 0;
+                preOp = c;
+                curNum = 0;
+                if (c == RIGHT) break;
             }
         }
-        int res = 0;
-        while (!stack.empty()) {
-            res = res + stack.peek();
-            stack.pop();
-        }
 
-        System.out.println(res);
+        res += preNum;
+        return res;
     }
 }
